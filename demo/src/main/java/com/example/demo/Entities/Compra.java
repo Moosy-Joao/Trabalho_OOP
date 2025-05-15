2d32c8a46305df49de4b794a704559154f0f7261
Joao Antonio <joao.souza.76@outlook.com>
2025-05-14 21:36:02 -0300

    Adição da Entidade Compra as entidades

---
+++ demo/src/main/java/com/example/demo/Entities/Compra.java
@@ -0,0 +1,83 @@
+package com.example.demo.Entities;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "compras")
+public class Compra {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long fornecedorId;
+
+    private Long produtoId;
+
+    private Integer quantidade;
+
+    private BigDecimal valorTotal;
+
+    private LocalDateTime dataCompra;
+
+    public Compra() {
+    }
+
+    public Compra(Long fornecedorId, Long produtoId, Integer quantidade, BigDecimal valorTotal, LocalDateTime dataCompra) {
+        this.fornecedorId = fornecedorId;
+        this.produtoId = produtoId;
+        this.quantidade = quantidade;
+        this.valorTotal = valorTotal;
+        this.dataCompra = dataCompra;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getFornecedorId() {
+        return fornecedorId;
+    }
+
+    public void setFornecedorId(Long fornecedorId) {
+        this.fornecedorId = fornecedorId;
+    }
+
+    public Long getProdutoId() {
+        return produtoId;
+    }
+
+    public void setProdutoId(Long produtoId) {
+        this.produtoId = produtoId;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public LocalDateTime getDataCompra() {
+        return dataCompra;
+    }
+
+    public void setDataCompra(LocalDateTime dataCompra) {
+        this.dataCompra = dataCompra;
+    }
+}
